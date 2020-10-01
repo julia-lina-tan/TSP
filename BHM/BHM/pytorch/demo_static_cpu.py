4ab827f22e1f030c764abd534a6bbfdcb2c9d22d
Julia <69620560+julia-lina-tan@users.noreply.github.com>
2020-10-01 19:36:30 +1000

    Update demo_static_cpu.py

--- BHM/BHM/pytorch/demo_static_cpu.py
+++ BHM/BHM/pytorch/demo_static_cpu.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import torch as pt
 import matplotlib.pyplot as pl
+#from bhmtorch_cpu import BHM2D_PYTORCH
 
 def getPartitions(cell_max_min, nPartx1, nPartx2):
     """
@@ -30,11 +31,11 @@ def load_parameters(case):
     parameters = \
         {filename: \
              ( os.path.abspath('../../Datasets/simulated/'+filename),
-              (1, 1), #hinge point resolution
-              (0, 200, -100, 150), #area [min1, max1, min2, max2]
+              (5, 5), #hinge point resolution
+              (-100, 100, 0, 100), #area [x1_min, x1_max, x2_min, x2_max] #NOTE: chnaged the range
               None,
               None,
-              0.5, #gamma
+              0.08, #gamma
               ),
 
          }
@@ -47,24 +48,23 @@ device = pt.device("cpu")
 # device = pt.device("cuda:0") # Uncomment this to run on GPU
 
 # Get the filename to read data points from
-filename = input("Input file: ")
+filename = 'static_test1' #input("Input file: ")
 
 # Read the file
 fn_train, cell_resolution, cell_max_min, _, _, gamma = load_parameters(filename)
 
 # Partition the environment into to 4 areas
 # TODO: We can parallelize this
-cell_max_min_segments = getPartitions(cell_max_min, 2, 2)
+cell_max_min_segments = getPartitions(cell_max_min, 1, 1) #NOTE: this is implemented to segment the environment and do mapping speperately. For now, let's consider that there's only one segment.
 
 # Read data
 print('\nReading '+fn_train+'.csv...')
 g = pd.read_csv(fn_train+'.csv', delimiter=', ').values[:, :]
 
 # Filter data
-layer = np.logical_and(g[:,2] >= 0.02, g[:,2] <= 0.125)
-#layer = np.logical_and(g[:, 2] >= -0.6, g[:, 2] <= -0.5)
+layer = g[:,0] <= 100 #NOTE: let's consider time frames < 100
 g = pt.tensor(g[layer, :], dtype=pt.float32)
-X = g[:, :2]
+X = g[:, 1:3] #NOTE: Previously we've read incorrect columns
 y = g[:, 3].reshape(-1, 1)
 # if pt.cuda.is_available():
 #     X = X.cuda()
@@ -84,7 +84,7 @@ for segi in range(len(cell_max_min_segments)):
     totalTime += (t2-t1)
 
     # query the model
-    q_resolution = 0.5
+    q_resolution = 1
     xx, yy= np.meshgrid(np.arange(cell_max_min[0], cell_max_min[1] - 1, q_resolution),
                          np.arange(cell_max_min[2], cell_max_min[3] - 1, q_resolution))
     grid = np.hstack((xx.ravel()[:, np.newaxis], yy.ravel()[:, np.newaxis]))
@@ -93,18 +93,20 @@ for segi in range(len(cell_max_min_segments)):
     toPlot.append((Xq,yq))
 print(' Total training time={} s'.format(np.round(totalTime, 2)))
 
-# Scatter plot raw data 
-pl.rcParams['figure.facecolor'] = 'white'
-pl.scatter(X[:,0], X[:,1], c=y, cmap='jet')
-
 # Plot occupancy map
 pl.close('all')
+pl.figure(figsize=(10,5))
+pl.subplot(121)
+# Scatter plot raw data
+pl.rcParams['figure.facecolor'] = 'white'
+pl.scatter(X[:,0], X[:,1], c=y, s=5, cmap='jet')
+pl.subplot(122)
 for segi in range(len(cell_max_min_segments)):
     ploti = toPlot[segi]
     Xq, yq = ploti[0], ploti[1]
     pl.scatter(Xq[:, 0], Xq[:, 1], c=yq, cmap='jet', s=5, vmin=0, vmax=1)
 pl.colorbar()
-pl.xlim([0,200]); pl.ylim([-100,150])
+#pl.xlim([0,200]); pl.ylim([-100,150])
 pl.title(filename)
 #pl.savefig(os.path.abspath('../../Outputs/'+filename+'.png'))
 pl.show()
